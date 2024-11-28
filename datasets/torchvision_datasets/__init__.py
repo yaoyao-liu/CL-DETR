952157f87fedf3c6417fc211a6016208120f8520
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2024-11-28 00:42:13 -0600

    Update __init__.py

--- datasets/torchvision_datasets/__init__.py
+++ datasets/torchvision_datasets/__init__.py
@@ -1,7 +1,1 @@
-# ------------------------------------------------------------------------
-# Deformable DETR
-# Copyright (c) 2020 SenseTime. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
-# ------------------------------------------------------------------------
-
 from .coco import CocoDetection
