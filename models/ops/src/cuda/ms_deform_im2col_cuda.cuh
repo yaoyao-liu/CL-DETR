2d6fc7e26f568baf5ced6ecef97488815016a01d
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2024-12-01 01:16:09 -0600

    Update ms_deform_im2col_cuda.cuh

--- models/ops/src/cuda/ms_deform_im2col_cuda.cuh
+++ models/ops/src/cuda/ms_deform_im2col_cuda.cuh
@@ -1,13 +1,3 @@
-/*!
-**************************************************************************
-* Deformable DETR
-* Copyright (c) 2020 SenseTime. All Rights Reserved.
-* Licensed under the Apache License, Version 2.0 [see LICENSE for details]
-**************************************************************************
-* Modified from DCN (https://github.com/msracver/Deformable-ConvNets)
-* Copyright (c) 2018 Microsoft
-**************************************************************************
-*/
 
 #include <cstdio>
 #include <algorithm>
